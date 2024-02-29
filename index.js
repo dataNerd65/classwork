a3e1e5f45116144556d678772994251de11b5399
King-web254 <vwanyungu254@gmail.com>
2024-02-29 09:08:40 +0300

    Cosmetics

--- index.js
+++ index.js
@@ -10,35 +10,35 @@ class Queue {
 	enqueue(item) {
 		this.items[this.backIndex] = item
 		this.backIndex++
-		return item + ' inserted'
+		return item + ' inserted Back'
 	}
     // Add an item to the front of the queue
     enqueueBack(item) {
         this.items[this.frontIndex - 1] = item
         this.frontIndex--
-        return item + ' inserted'
+        return item + ' inserted Front'
     }
     // Remove an item from the front of the queue
 	dequeue() {
 		const item = this.items[this.frontIndex]
 		delete this.items[this.frontIndex]
 		this.frontIndex++
-		return item
+		return item  + ' removed Front'
 	}
     // Remove an item from the back of the queue
     dequeueBack() {
         const item = this.items[this.backIndex - 1]
         delete this.items[this.backIndex - 1]
         this.backIndex--
-        return item
+        return item + ' removed Back'
     }
     // First item
 	front() {
-		return this.items[this.frontIndex]
+		return this.items[this.frontIndex] + ' is the first item'
 	}
     // Last item
     back() {
-        return this.items[this.backIndex - 1]
+        return this.items[this.backIndex - 1] + ' is the last item'
     }
     // Returns the queue
 	get printQueue() {
@@ -68,15 +68,19 @@ const myList = [1, 2, 3, 4, 5]
 
 // Adding an element to the end of the list
 myList.push(6)
+console.log("Added 6 to the end of the list")
 
 // Removing an element from the end of the list
 myList.pop()
+console.log("Removing an element from the end of the list")
 
 // Adding an element to the front of the list
 myList.unshift(5)
+console.log("Added 5 to the front of the list")
 
 // Removing an element from the front of the list
 myList.shift()
+console.log("Removing an element from the front of the list")
 
 // Accessing elements by index
 console.log(myList[2]); // Output: 3
